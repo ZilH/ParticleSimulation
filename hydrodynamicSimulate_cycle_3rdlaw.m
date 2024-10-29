1415cf074227380c3b3e61d0ef69316be5d5f750
Zilong He <andyhezilong@gmail.com>
2024-10-29 09:29:03 -0500

    add lubrication force

--- hydrodynamicSimulate_cycle_3rdlaw.m
+++ hydrodynamicSimulate_cycle_3rdlaw.m
@@ -2,11 +2,18 @@ clear
 close all
 
 % Define simulation constants
-N_cycles = 50;
+tic
+
+N_cycles = 10;
 
 makeVideo = true;
 continuousVideo = false;
 markersize = 45;
+
+isLubrication = true;
+
+isLoadTestXY = true;
+savedata = false;
 
 %% Physical parameters
 
@@ -68,13 +75,13 @@ box_width = (num_particles * pi * d) / (4 * aspect_ratio * phi);
 % Generate random positions for the particles
 particle_x = rand(num_particles, 1) * box_width;
 particle_y = rand(num_particles, 1) * (box_height - d) + d/2;
+% particle_y = rand(num_particles, 1) * box_height;
+
+if isLoadTestXY
+    load('test_xy_location.mat');
+end
 
 R = box_height / 2;
-
-% Generate random positions for the particles
-particle_x = rand(num_particles, 1) * box_width;
-% particle_y = rand(num_particles, 1) * box_height;
-particle_y = rand(num_particles, 1) * (box_height - d) + d/2;
 
 % Plot the particles as circles
 % init_x = particle_x;
@@ -91,9 +98,13 @@ msd_y_cycle = zeros(N_cycles,1);
 
 % Set up video writer
 if makeVideo
-    vidName = sprintf('Test_2DSimulation_Q%03d_3rdLaw_nondim',Q*1000);
+    if isLubrication
+        vidName = sprintf('Test_2DSimulation_Q%03d_3rdLaw_nondim_lub',Q*1000);
+    else
+        vidName = sprintf('Test_2DSimulation_Q%03d_3rdLaw_nondim',Q*1000);
+    end
     vidObj = VideoWriter(vidName);
-    vidObj.FrameRate = 15;  % Set the frame rate
+    vidObj.FrameRate = 10;  % Set the frame rate
     open(vidObj);
 end
 
@@ -117,32 +128,32 @@ for it = 1:iters
     else
         Vc = -Vc_magnitude;  % Negative for iterations
     end
-    
-    particle_x_forward = 0 * particle_x;
-    
-    for i = 1:num_particles
-        particle_x_forward(i) = particle_x(i) + Vc * (1 - (particle_y(i) - R)^2/R^2) * dt;
-    end
-    
-    % Check Collision before shear
-    D1 = pdist([particle_x,particle_y]);
-    Dsq = squareform(D1);
-    ind = find(Dsq < d & Dsq ~= 0);
-    % Convert linear indices to row and column indices
-    [nrows, ncols] = size(Dsq);
-    [row_ind_0, col_ind] = ind2sub([nrows, ncols], ind);
-    
-    % Check Collision after shear
-    D1 = pdist([particle_x_forward,particle_y]);
-    Dsq = squareform(D1);
-    ind = find(Dsq < d & Dsq ~= 0);
-    % Convert linear indices to row and column indices
-    [nrows, ncols] = size(Dsq);
-    [row_ind_1, col_ind] = ind2sub([nrows, ncols], ind);
-    
-    
-    row_ind = unique([row_ind_0;row_ind_1]);
-    active(it) = length(row_ind);
+%     
+%     particle_x_forward = 0 * particle_x;
+%     
+%     for i = 1:num_particles
+%         particle_x_forward(i) = particle_x(i) + Vc * (1 - (particle_y(i) - R)^2/R^2) * dt;
+%     end
+%     
+%     % Check Collision before shear
+%     D1 = pdist([particle_x,particle_y]);
+%     Dsq = squareform(D1);
+%     ind = find(Dsq < d & Dsq ~= 0);
+%     % Convert linear indices to row and column indices
+%     [nrows, ncols] = size(Dsq);
+%     [row_ind_0, col_ind] = ind2sub([nrows, ncols], ind);
+%     
+%     % Check Collision after shear
+%     D1 = pdist([particle_x_forward,particle_y]);
+%     Dsq = squareform(D1);
+%     ind = find(Dsq < d & Dsq ~= 0);
+%     % Convert linear indices to row and column indices
+%     [nrows, ncols] = size(Dsq);
+%     [row_ind_1, col_ind] = ind2sub([nrows, ncols], ind);
+%     
+%     
+%     row_ind = unique([row_ind_0;row_ind_1]);
+%     active(it) = length(row_ind);
     
 %     figure
 %     set(gcf, 'Position',  [100, 100, 1200, 400])
@@ -241,45 +252,145 @@ for it = 1:iters
 %     total_F_c_y = sum(F_c_y);
 %     fprintf('Total contact force in x-direction: %e\n', total_F_c_x);
 %     fprintf('Total contact force in y-direction: %e\n', total_F_c_y);
-    
-    for i = 1:num_particles
-        % Compute background flow velocity at particle i using parabolic profile
-%         u_inf_i_x = Vc * (1 - ((particle_y(i) - R) / R)^2);
-        u_inf_i_x = Vc * particle_y(i) * (2 * R - particle_y(i)) / R^2;
-        u_inf_i_y = 0; % No flow in y-direction
-        
-        % Calculate particle velocity from force balance
-%         u_i_x = u_inf_i_x - (1 / hydro_coeff) * F_c_x(i);
-%         u_i_y = u_inf_i_y - (1 / hydro_coeff) * F_c_y(i);
-        u_i_x = u_inf_i_x - F_c_x(i);
-        u_i_y = u_inf_i_y - F_c_y(i);
-        
-        
-        % Update particle positions
-        particle_x(i) = particle_x(i) + u_i_x * dt;
-        particle_y(i) = particle_y(i) + u_i_y * dt;
-        
-%         msd_x(it) = msd_x(it) + (u_i_x * dt)^2;
-%         msd_y(it) = msd_y(it) + (u_i_y * dt)^2;
-                
-        % No penetration boundary condition:
-        if is_upbotwallHard
-            if particle_y(i) >= box_height - d/2
-                particle_y(i) = box_height - d/2;
-            elseif particle_y(i) < d/2
-                particle_y(i) = d/2;
-            end
+
+    if isLubrication
+        N = num_particles;
+        % Initialize A and b
+        A = zeros(2*N, 2*N);
+        b = zeros(2*N, 1);
+
+        % Loop over particles to set up drag and contact forces
+        for i = 1:N
+            idx_i_x = 2*i - 1;
+            idx_i_y = 2*i;
+
+            % Background flow velocity at particle i
+            u_inf_i_x = Vc * particle_y(i) * (2 * R - particle_y(i)) / R^2;
+            u_inf_i_y = 0;
+
+            % Hydrodynamic drag coefficients
+            K = 1;
+
+            % Diagonal terms (drag)
+            A(idx_i_x, idx_i_x) = K;
+            A(idx_i_y, idx_i_y) = K;
+
+            % Right-hand side (drag against background flow and contact forces)
+            b(idx_i_x) = K * u_inf_i_x - F_c_x(i);
+            b(idx_i_y) = K * u_inf_i_y - F_c_y(i);
         end
         
-        % Bounding boundary condition :
-        if is_upbotwallBounce
-            if particle_y(row_ind(idx)) >= box_height - d/2
-                particle_y(row_ind(idx)) = box_height - d/2 - epsilon * rand;
-            elseif particle_y(row_ind(idx)) < d/2
-                particle_y(row_ind(idx)) = d/2 + epsilon * rand;
+        % Loop over unique particle pairs for lubrication forces
+        for i = 1:N-1
+            for j = i+1:N
+                dx = particle_x(j) - particle_x(i);
+                dy = particle_y(j) - particle_y(i);
+                x_ij = sqrt(dx^2 + dy^2);
+
+                h_ij = x_ij - d;
+
+                if h_ij > 0 && h_ij < epsilon % epsilon is a small cutoff value
+                    % Lubrication coefficient
+                    L_ij = 1 / (8 * h_ij);
+
+                    % Unit vector
+                    n_ij_x = dx / x_ij;
+                    n_ij_y = dy / x_ij;
+
+                    idx_i_x = 2*i - 1;
+                    idx_i_y = 2*i;
+                    idx_j_x = 2*j - 1;
+                    idx_j_y = 2*j;
+
+                    % Lubrication terms
+                    % For particle i
+                    A(idx_i_x, idx_i_x) = A(idx_i_x, idx_i_x) + L_ij * n_ij_x^2;
+                    A(idx_i_x, idx_i_y) = A(idx_i_x, idx_i_y) + L_ij * n_ij_x * n_ij_y;
+                    A(idx_i_y, idx_i_x) = A(idx_i_y, idx_i_x) + L_ij * n_ij_x * n_ij_y;
+                    A(idx_i_y, idx_i_y) = A(idx_i_y, idx_i_y) + L_ij * n_ij_y^2;
+
+                    % For particle j
+                    A(idx_j_x, idx_j_x) = A(idx_j_x, idx_j_x) + L_ij * n_ij_x^2;
+                    A(idx_j_x, idx_j_y) = A(idx_j_x, idx_j_y) + L_ij * n_ij_x * n_ij_y;
+                    A(idx_j_y, idx_j_x) = A(idx_j_y, idx_j_x) + L_ij * n_ij_x * n_ij_y;
+                    A(idx_j_y, idx_j_y) = A(idx_j_y, idx_j_y) + L_ij * n_ij_y^2;
+
+                    % Coupling terms
+                    A(idx_i_x, idx_j_x) = A(idx_i_x, idx_j_x) - L_ij * n_ij_x^2;
+                    A(idx_i_x, idx_j_y) = A(idx_i_x, idx_j_y) - L_ij * n_ij_x * n_ij_y;
+                    A(idx_i_y, idx_j_x) = A(idx_i_y, idx_j_x) - L_ij * n_ij_x * n_ij_y;
+                    A(idx_i_y, idx_j_y) = A(idx_i_y, idx_j_y) - L_ij * n_ij_y^2;
+
+                    % Symmetric entries
+                    A(idx_j_x, idx_i_x) = A(idx_j_x, idx_i_x) - L_ij * n_ij_x^2;
+                    A(idx_j_x, idx_i_y) = A(idx_j_x, idx_i_y) - L_ij * n_ij_x * n_ij_y;
+                    A(idx_j_y, idx_i_x) = A(idx_j_y, idx_i_x) - L_ij * n_ij_x * n_ij_y;
+                    A(idx_j_y, idx_i_y) = A(idx_j_y, idx_i_y) - L_ij * n_ij_y^2;
+                end
             end
         end
+        u = A \ b;
+
+        % Extract velocities
+        for i = 1:N
+            idx_i_x = 2*i - 1;
+            idx_i_y = 2*i;
+
+            u_i_x = u(idx_i_x);
+            u_i_y = u(idx_i_y);
+
+            % Update particle positions
+            particle_x(i) = particle_x(i) + u_i_x * dt;
+            particle_y(i) = particle_y(i) + u_i_y * dt;
+
+            % Apply boundary conditions...
+            if is_upbotwallHard
+                if particle_y(i) >= box_height - d/2
+                    particle_y(i) = box_height - d/2;
+                elseif particle_y(i) < d/2
+                    particle_y(i) = d/2;
+                end
+            end
+        end
+    else
+        for i = 1:num_particles
+            % Compute background flow velocity at particle i using parabolic profile
+            %         u_inf_i_x = Vc * (1 - ((particle_y(i) - R) / R)^2);
+            u_inf_i_x = Vc * particle_y(i) * (2 * R - particle_y(i)) / R^2;
+            u_inf_i_y = 0; % No flow in y-direction
+            
+            % Calculate particle velocity from force balance
+            u_i_x = u_inf_i_x - F_c_x(i);
+            u_i_y = u_inf_i_y - F_c_y(i);
+            
+            
+            % Update particle positions
+            particle_x(i) = particle_x(i) + u_i_x * dt;
+            particle_y(i) = particle_y(i) + u_i_y * dt;
+            
+            %         msd_x(it) = msd_x(it) + (u_i_x * dt)^2;
+            %         msd_y(it) = msd_y(it) + (u_i_y * dt)^2;
+            
+            % No penetration boundary condition:
+            if is_upbotwallHard
+                if particle_y(i) >= box_height - d/2
+                    particle_y(i) = box_height - d/2;
+                elseif particle_y(i) < d/2
+                    particle_y(i) = d/2;
+                end
+            end
+            
+            % Bounding boundary condition :
+            %         if is_upbotwallBounce
+            %             if particle_y(row_ind(idx)) >= box_height - d/2
+            %                 particle_y(row_ind(idx)) = box_height - d/2 - epsilon * rand;
+            %             elseif particle_y(row_ind(idx)) < d/2
+            %                 particle_y(row_ind(idx)) = d/2 + epsilon * rand;
+            %             end
+            %         end
+        end        
     end
+    
     
 %     msd_x(it) = msd_x(it) + (particle_x - init_x)^2;
 %     msd_y(it) = msd_y(it) + (particle_y - init_y)^2;
@@ -296,6 +407,7 @@ for it = 1:iters
     if mod(it, 2*half_cycle_iters) == 0  % End of a cycle
         msd_x_cycle(it / (2*half_cycle_iters)) = mean((particle_x - init_x_cycle).^2);
         msd_y_cycle(it / (2*half_cycle_iters)) = mean((particle_y - init_y_cycle).^2);
+        toc
     end
 end
 % Close video writer
@@ -315,6 +427,10 @@ end
 % end
 
 % plot(msd_x_cycle)
-if ~continuousVideo
-    save(sprintf('./NoLubricationRes/Q%d.mat',Q*1000))
+if ~continuousVideo && savedata
+    if ~isLubrication
+        save(sprintf('./NoLubricationRes/Q%d.mat',Q*1000))
+    else
+        save(sprintf('./LubricationRes/Q%d.mat',Q*1000))
+    end
 end
